ecf885af3a277002c3521e6d41a15606e170fbb7
ddialar <ddialar@gmail.com>
2022-07-11 11:25:11 +0100

    feat(authentication): implement the NextAuth callback functions

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -11,5 +11,19 @@ export default NextAuth({
   ],
   pages: {
     signIn: '/signin'
+  },
+  callbacks: {
+    async signIn ({ user, account, profile, email, credentials }) {
+      return true
+    },
+    async redirect ({ url, baseUrl }) {
+      return baseUrl
+    },
+    async session ({ session, user, token }) {
+      return session
+    },
+    async jwt ({ token, user, account, profile, isNewUser }) {
+      return token
+    }
   }
 })
